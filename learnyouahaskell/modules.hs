353db8ee8a09b519e4474b007d94c8eca5e0d090
Devin McGloin <devin@devinmcgloin.com>
2015-06-15 18:40:11 -0400

    added learn you a haskell info and notes of speed

---
+++ learnyouahaskell/modules.hs
@@ -0,0 +1,11 @@
+{-- http://learnyouahaskell.com/modules --}
+import Data.List
+--import Data.List hiding (nub)
+--import qualified Data.List as M
+
+
+main :: IO ()
+main = do
+   print $
+
+numUniques = length . nub
