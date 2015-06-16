8c5b90e913b000c9d6ccb5439bf247ef6ad65f46
Devin McGloin <devin@devinmcgloin.com>
2015-06-16 00:37:15 -0400

    real world haskell CH04

--- realworldhaskell/ch03/assign.hs
+++ realworldhaskell/ch03/assign.hs
@@ -75,7 +75,13 @@ depth (Node a b c) = 1 + max (depth b) (depth c)
 
 {--Consider three two-dimensional points a, b, and c. If we look at the angle formed by the line segment from a to b and the line segment from b to c, it either turns left, turns right, or forms a straight line. Define a Direction data type that lets you represent these possibilities. 24 comments--}
 
+data Direction = Strait | Left | Right
+      deriving (Show)
 
+data Point2D = Cartesian2D {
+	  x :: Double
+	, y :: Double
+	} deriving (Eq, Show)
 
 {-- Write a function that calculates the turn made by three 2D points and returns a Direction. 58 comments --}
 
